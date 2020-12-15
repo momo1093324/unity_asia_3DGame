7ed2df88b91e87fafe4a9e5c79fac353ea6d9128
momo1093324 <71679878+momo1093324@users.noreply.github.com>
2020-12-15 16:00:52 +0800

    npc程式 對話框輸入

---
+++ unity_asia_3DGame/Assets/Scripts/NPC.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class NPC : MonoBehaviour
+{
+    [Header("NPC資料")]
+    public NPCDate date ;
+    [Header("對話框")]
+    public GameObject dialog;
+    [Header("對話內容")]
+    public Text textContent;
+
+    //玩家是否進入感應區
+    public bool playerInArea;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.name=="小明")
+        {
+            playerInArea = true;
+            Dialog();
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.name == "小明")
+        {
+            playerInArea = false;
+
+        }
+  }
+    private void Dialog()
+    {
+        //print(date.dialougA);  取得字串全部資料
+        // print(date.dialougA[2]);  取得字串局部資料 ex.[2]為字串第三個字
+
+        //for迴圈-重複處理相同程式
+        //for (int i = 0; i < 6; i++)
+        //{
+        //    print("我是迴圈:" + i);
+        //}
+        for (int i = 0; i < date.dialougA.Length; i++)//Length包含所有文字
+        {
+            print(date.dialougA[ i]);
+        }
+    }
+}
+
---
+++ unity_asia_3DGame/Assets/Scripts/NPCDate.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName ="NPC 資料",menuName="a1070501005/NPC 資料")]
+//建立資源選單
+public class NPCDate : ScriptableObject
+//ScriptableObject腳本(實體)化物件
+{
+    [Header("第一段對話"),TextArea(1,5)]
+    public string dialougA;
+    [Header("第二段對話"), TextArea(1, 5)]
+    public string dialougB;
+    [Header("第三段對話"), TextArea(1, 5)]
+    public string dialougC;
+    [Header("任務項目需求數量")]
+    public int count;
+    [Header("已經取得項目數量")]
+    public int countCurrent;
+}
